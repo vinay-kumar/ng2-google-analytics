e34754a7fca8e613032004b09ba91bd77fee4921
vinay mac <vinay@mobicomodo.com>
2020-05-08 11:47:23 +0530

    Added new feature tracking

--- README.md
+++ README.md
@@ -5,13 +5,14 @@
 2. Angular 2+ and Ionic 2+ compatible
 3. Track traditional Ionic 2 Pages with no URL Change (use setPage(pageName) function )
 4. Track Custom Events with trackEvent function
+5. Add multiple tracker IDs
 
 ## to build the library
 ng build ng2-google-analytics
 
 ## steps to integrate
 
-### Add Ng2GoogleAnalytics in app.module.ts
+### Add `Ng2GoogleAnalytics` in app.module.ts
 
 ```javascript
 imports: [
@@ -38,7 +39,7 @@ constructor(
   }
 ```
 
-### call setPage(pageName) function to track pages
+### call `setPage(pageName)` function to track pages
 ```javascript
 // add in constructor
 constructor(
@@ -52,19 +53,55 @@ constructor(
  }
 ```
 
-### call trackEvent() function to track events
-```javascript
+### call `trackEvent()` or `trackEventV2()` function to track events
 
-    this.ga.trackEvent('Register', 'Submit', 'Register Button', 1);
+```javascript
+    this.ga.trackEvent('Register', 'Submit');
+
+    // or call trackEvent2 which takes first param as object
+    this.ga.trackEventV2({
+      eventCategory: 'Register', 
+      eventAction: 'Submit', 
+    });
+    
+    // or call like this 
+    this.ga.trackEventV2({
+      eventCategory: 'Register', 
+      eventAction: 'Submit', 
+      eventLabel: 'Register Button',
+      eventValue: 1
+    });
 
 ```
 
+
+## Additional Features
 ### to disable GA while development
 send second parameter of initialize function to false.
 It may be driven from environment.ts file.
 See https://angular.io/guide/build#configuring-application-environments
 ```javascript
     ga.initialize('UA-XXXXXXX-1', environment.production);
+```
+
+### set additional tracker ID
+Sometimes you need to set more than one tracking ID with your project.
+This can be done with `setAdditionalTraker` function.
+
+```javascript
+  //call this function to set additional tracker
+  this.ga.setAdditionalTraker('UA-XXXXXX-2', 'myCustomTrackerName')
+
+  // if you want to send page to different tacking id
+  this.ga.setPage('register.html', 'myCustomTrackingName');
+
+  //if you want to send event to different tacking id
+  this.ga.trackEventV2({
+    eventCategory: 'Register', 
+    eventAction: 'Submit', 
+    eventLabel: 'Register Button',
+    eventValue: 1
+  }, 'myCustomTrackingName');
 ```
 
 Tested on Angular 7
--- projects/ng2-google-analytics/src/lib/ng2-google-analytics.service.ts
+++ projects/ng2-google-analytics/src/lib/ng2-google-analytics.service.ts
@@ -1,17 +1,51 @@
 import { Injectable } from '@angular/core';
 declare var ga: Function;
 
+export class GAEventOptions {
+
+  /**
+   * @required Typically the object that was interacted with (e.g. 'Video')
+   */
+  eventCategory:string = '';
+
+  /**
+   * @required The type of interaction (e.g. 'play')
+   */
+  eventAction:string = '';
+
+  /**
+   * Useful for categorizing events (e.g. 'Fall Campaign')
+   */
+  eventLabel?:string = null;
+
+  /**
+   * A numeric value associated with the event (e.g. 42)
+   */
+  eventValue?:number = 0;
+
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class Ng2GoogleAnalyticsService {
   public trackerId: string;
   public isProd: boolean = true;
+  public additionalTrackers = {};
+
+  private scriptAdded: boolean = false;
+
   constructor() {
   }
 
-  addGaScript() {
+  private addGaScript() {
     try {
+      if (this.scriptAdded) {
+        //script is already added skip adding it again.
+        console.error('duplicate call to add google analytics tag')
+        return;
+      }
       const script = document.createElement('script');
       script.innerHTML = `
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -22,12 +56,31 @@ export class Ng2GoogleAnalyticsService {
       `;
       document.head.appendChild(script);
       ga('create', this.trackerId);
+      this.scriptAdded = true;
     } catch (error) {
       console.error('ga script add', error);
     }
   }
 
+  /**
+   *
+   * @param trackerId String 'UA-XXXXX-Y' additional tracker Id to attach
+   * @param trackerName String unique name for trackerId (e.g. clientTracker) to identify tracker
+   */
+  setAdditionalTraker(trackerId: string, trackerName: string) {
+    if (this.additionalTrackers[trackerName]) {
+      return false;
+    }
+    this.additionalTrackers[trackerName] = trackerId;
+    ga('create', trackerId, 'auto', trackerName);
+    return true;
+  }
 
+/**
+ * Initialize GA. Should be called only once.
+ * @param trackedId Google Analytics Tracker ID UA-XXXXXXXXX-1
+ * @param isProd link google analytics default true, set false stopping analytics when developing
+ */
   initialize(trackedId: string, isProd: boolean = true) {
     this.trackerId = trackedId;
     if (isProd === false) {
@@ -35,32 +88,74 @@ export class Ng2GoogleAnalyticsService {
     } else {
       this.addGaScript();
     }
-
   }
 
-  setPage(pageName: string) {
+  /**
+   * SetPage function
+   * useful in applications with no URL change or
+   * setting custom name for page
+   * @param pageName Name of the page r.g. Register or register.html
+   */
+  setPage(pageName: string, trackerName: string = null) {
     if (!this.isProd) {
       return;
     }
     try {
-      ga('set', 'page', '/' + pageName);
-      ga('send', 'pageview');
+      if (trackerName && this.additionalTrackers[trackerName]) {
+        ga(trackerName + '.set', 'page', '/' + pageName);
+        ga(trackerName + '.send', 'pageview');
+      } else {
+        ga('set', 'page', '/' + pageName);
+        ga('send', 'pageview');
+      }
     } catch (error) {
       console.error('Error while setting page');
     }
   }
 
-  trackEvent(eventCategory: string, eventAction: string, eventLabel: string = null, eventValue: number = null) {
+  /**
+   * Function to send event to trackerId
+   * @param eventParams GAEventOptions
+   * @param trackerName optional, to send event to different tracker
+   */
+  trackEventV2(eventParams: GAEventOptions, trackerName: string = null) {
     if (!this.isProd) {
       return;
     }
     try {
-      ga('send', 'event', {
-        eventCategory: eventCategory,
-        eventLabel: eventLabel,
-        eventAction: eventAction,
-        eventValue: eventValue
-      });
+      ga((trackerName && this.additionalTrackers[trackerName]) ? trackerName + '.send' : 'send', 'event', eventParams);
+    } catch (error) {
+      console.error('Error while sending event');
+    }
+  }
+
+  /**
+   * Function to track any event trigger on App (e.g Clicking the Video button)
+   * @param eventCategory Typically the object that was interacted with (e.g. 'Video')
+   * @param eventAction The type of interaction (e.g. 'play')
+   * @param eventLabel Useful for categorizing events (e.g. 'Fall Campaign')
+   * @param eventValue 	A numeric value associated with the event (e.g. 42)
+   * @deprecated will be removed in next major version
+   */
+  trackEvent(eventCategory: string, eventAction: string, eventLabel: string = null, eventValue: number = 0) {
+    if (!this.isProd) {
+      return;
+    }
+    let eventObj = {
+      eventCategory: eventCategory,
+      eventAction: eventAction
+    };
+
+    if (eventLabel) {
+      eventObj['eventLabel'] = eventLabel;
+    }
+
+    if (eventValue) {
+      eventObj['eventValue'] = eventValue;
+    }
+
+    try {
+      ga('send', 'event', eventObj);
     } catch (error) {
       console.error('Error while sending event');
     }
